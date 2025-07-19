0c76606ad0e84aca0aea94612d230d4fdc3fdf36
pokycookie <0cookieboy0@gmail.com>
2025-07-19 16:52:38 +0900

    refactor: data table 리팩토링

---
+++ src/components/data-table.tsx
@@ -0,0 +1,82 @@
+import { cn } from '@/lib/utils'
+import { flexRender, getCoreRowModel, useReactTable, type ColumnDef } from '@tanstack/react-table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+
+interface DataTableProps<TData, TValue> {
+  className?: string
+  data: TData[]
+  columns: ColumnDef<TData, TValue>[]
+}
+
+export function DataTable<TData, TValue>({
+  className,
+  data,
+  columns,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className={cn('overflow-hidden border rounded-lg', className)}>
+      <div className="overflow-y-auto w-full h-full">
+        <Table className="w-full">
+          <TableHeader className="h-14">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className="bg-muted">
+                {headerGroup.headers.map((header) => (
+                  <TableHead
+                    key={header.id}
+                    style={{
+                      ...(header.column.columnDef.size !== undefined && {
+                        width: `${header.getSize()}px`,
+                        minWidth: `${header.getSize()}px`,
+                        maxWidth: `${header.getSize()}px`,
+                        flexShrink: 0,
+                      }),
+                    }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      style={{
+                        ...(cell.column.columnDef.size !== undefined && {
+                          width: `${cell.column.getSize()}px`,
+                          minWidth: `${cell.column.getSize()}px`,
+                          maxWidth: `${cell.column.getSize()}px`,
+                          flexShrink: 0,
+                        }),
+                      }}
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
--- src/views/admin/worlds/components/world-table/world-data-table.tsx
+++ src/views/admin/worlds/components/world-table/world-data-table.tsx
@@ -1,17 +1,12 @@
 import { useAllGetWorlds, useUpdateeWorldActivate } from '@/api/hooks/worlds'
 import { Button } from '@/components/ui/button'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import { cn } from '@/lib/utils'
 import { CircleCheck, CirclePause, Globe } from 'lucide-react'
 import { UpdateWorldDialogTrigger } from '@/views/admin/worlds/components'
+import type { ColumnDef } from '@tanstack/react-table'
+import type { World } from '@/entities/worlds'
+import { DataTable } from '@/components/data-table'
+import { cn } from '@/lib/utils'
 
 interface WorldDataTableProps {
   className?: string
@@ -21,80 +16,79 @@ export function WorldDataTable({ className }: WorldDataTableProps) {
   const { worlds, isLoading } = useAllGetWorlds()
   const { activateWorld, deactivateWorld } = useUpdateeWorldActivate()
 
-  return (
-    <div className={cn('overflow-hidden border rounded-lg', className)}>
-      <div className="overflow-y-auto w-full h-full">
-        <Table>
-          <TableHeader className="h-14">
-            <TableRow className="bg-muted">
-              <TableHead className="w-16">ID</TableHead>
-              <TableHead className="w-10"></TableHead>
-              <TableHead>월드명</TableHead>
-              <TableHead className="w-16 text-center">상태</TableHead>
-              <TableHead className="w-0"></TableHead>
-              <TableHead className="w-0"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {worlds.map((world) => (
-              <TableRow key={world.id} className="h-12">
-                <TableCell>{world.id}</TableCell>
-                <TableCell>
-                  <div className="flex justify-center items-center">
-                    {world.logo ? (
-                      <img src={world.logo} alt={world.name} className="size-5" />
-                    ) : (
-                      <Globe />
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell>{world.name}</TableCell>
-                <TableCell>
-                  <div className="flex justify-center items-center">
-                    {world.isActive ? (
-                      <CircleCheck className="size-4 text-green-600" />
-                    ) : (
-                      <CirclePause className="size-4 text-red-500" />
-                    )}
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <UpdateWorldDialogTrigger data={world} />
-                </TableCell>
-                <TableCell>
-                  {world.isActive ? (
-                    <Tooltip disableHoverableContent>
-                      <TooltipTrigger asChild>
-                        <Button
-                          variant="outline"
-                          size="icon"
-                          onClick={() => deactivateWorld(world.id)}
-                        >
-                          <CirclePause className="size-4 text-red-500" />
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent>월드 비활성화</TooltipContent>
-                    </Tooltip>
-                  ) : (
-                    <Tooltip disableHoverableContent>
-                      <TooltipTrigger asChild>
-                        <Button
-                          variant="outline"
-                          size="icon"
-                          onClick={() => activateWorld(world.id)}
-                        >
-                          <CircleCheck className="size-4 text-green-600" />
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent>월드 활성화</TooltipContent>
-                    </Tooltip>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
-  )
+  const columns: ColumnDef<World>[] = [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+      size: 10,
+    },
+    {
+      header: '',
+      accessorKey: 'logo',
+      size: 10,
+      cell: ({ row }) => {
+        const name = row.original.name
+        const logo = row.original.logo
+        return (
+          <div className="flex justify-center items-center">
+            {logo ? <img src={logo} alt={name} className="size-5" /> : <Globe />}
+          </div>
+        )
+      },
+    },
+    {
+      header: '월드명',
+      accessorKey: 'name',
+    },
+    {
+      accessorKey: 'isActive',
+      header: () => <div className="w-full text-center">상태</div>,
+      size: 30,
+      cell: ({ row }) => {
+        const isActive = row.original.isActive
+        return (
+          <div className="flex justify-center items-center">
+            {isActive ? (
+              <CircleCheck className="size-4 text-green-600" />
+            ) : (
+              <CirclePause className="size-4 text-red-500" />
+            )}
+          </div>
+        )
+      },
+    },
+    {
+      id: 'actions',
+      size: 40,
+      cell: ({ row }) => {
+        const world = row.original
+        return (
+          <div className="flex gap-2 shrink-0">
+            <UpdateWorldDialogTrigger data={row.original} />
+            {world.isActive ? (
+              <Tooltip disableHoverableContent>
+                <TooltipTrigger asChild>
+                  <Button variant="outline" size="icon" onClick={() => deactivateWorld(world.id)}>
+                    <CirclePause className="size-4 text-red-500" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>월드 비활성화</TooltipContent>
+              </Tooltip>
+            ) : (
+              <Tooltip disableHoverableContent>
+                <TooltipTrigger asChild>
+                  <Button variant="outline" size="icon" onClick={() => activateWorld(world.id)}>
+                    <CircleCheck className="size-4 text-green-600" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>월드 활성화</TooltipContent>
+              </Tooltip>
+            )}
+          </div>
+        )
+      },
+    },
+  ]
+
+  return <DataTable columns={columns} data={worlds} className={cn(className)} />
 }
